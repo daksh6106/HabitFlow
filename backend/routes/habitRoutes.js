3aa7695f9d701f0d8021ab3a8c9e599e5027b2a1
Daksh Sharma <daksh6106@gmail.com>
2026-07-24 22:57:41 +0530

    Fix Habit import

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Habit = require("../models/Habit");
+const Habit = require("../models/habit");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const { 
