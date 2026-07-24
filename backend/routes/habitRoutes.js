12d961b0bf2b21ea47005b318301a54283878451
Daksh Sharma <daksh6106@gmail.com>
2026-07-24 22:54:07 +0530

    Fix model import

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Habit = require("../models/Habit");
-const User = require("../models/User");
+const User = require("../models/user");
 const auth = require("../middleware/auth");
 const { 
     updateUserStreak,
